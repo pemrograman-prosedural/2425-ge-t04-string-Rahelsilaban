8a002fa697ba35770030b68b198fbb603bac62ad
Rahelsilaban <rahelj.e.silaban23@gmail.com>
2025-02-27 14:08:39 +0700

    Pair 01 telah menyelesaikan tugas praktikum

--- t04_01.c
+++ t04_01.c
@@ -1,10 +1,25 @@
-// NIM - Name
-// NIM - Name
+// 12S4007 - Dasmauli Sormin
+// 12S24054 - Rahel Juri Elisabet Silaban
 
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 int main(int _argv, char **_argc) {
-  // codes
+  
+  char msk[61];
+  int r=0;
+  int ASCII;
+  char ASCII2;
+
+  scanf("%60s", msk);
+  while (r< strlen(msk)- 2){
+    sscanf(&msk[r], "%3d", &ASCII);
+    ASCII2 = (char) ASCII;
+    printf("%c", ASCII2);
+    r += 3;
+  }
+  printf("\n");
 
   return 0;
 }
--- t04_02.c
+++ t04_02.c
@@ -1,10 +1,20 @@
-// NIM - Name
-// NIM - Name
+// 12S24007 - Dasmauli Sormin
+// 12S24054 - Rahel Juri Elisabet Silaban
 
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 int main(int _argv, char **_argc) {
-  // codes
+  char msk[61];
+  int j;
+
+  fgets(msk, 61, stdin); // membaca input
+
+  for (j = 0; j < strlen (msk) - 1 ; j++){ // menampilkan ASCII dalam format tiga digit
+    printf("%03d", msk[j]);
+  }
 
   return 0;
-}
+} 
+
